7df737fe6d610b05d77482467d8171f33ddfa8ef
Ferhof <alexander.kiriltsev@gmail.com>
2026-02-14 18:44:59 +0300

    dz-4

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+type BinList []Bin
+type Bin struct {
+	id         string
+	private    bool
+	created_at time.Time
+	name       string
+}
+
+func main() {
+	bin1 := Bin{
+		id:         "123",
+		private:    false,
+		created_at: time.Now(),
+		name:       "example",
+	}
+
+	bin2 := Bin{
+		id:         "456",
+		private:    true,
+		created_at: time.Now(),
+		name:       "test",
+	}
+
+	binList := BinList{bin1}
+	binList = append(binList, bin2)
+
+	fmt.Println(binList)
 }
