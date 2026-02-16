00ef1b065d3d47bdf296e2dfd762374fdeccc880
Ferhof <alexander.kiriltsev@gmail.com>
2026-02-16 21:14:20 +0300

    dz5: Доработан file.go

--- file/file.go
+++ file/file.go
@@ -1,1 +1,32 @@
 package file
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+func ReadFile(filename string) ([]byte, error) {
+	fileData, err := os.ReadFile("data.json")
+	if err != nil {
+		return nil, errors.New("файл для чтения не найден")
+	}
+
+	return fileData, nil
+}
+
+func IsJsonFile(filename string) bool {
+	fileData, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Ошибка чтения файла")
+		return false
+	}
+	var data interface{}
+	decoder := json.NewDecoder(fileData)
+	if err := decoder.Decode(&data); err != nil {
+		fmt.Println("JSON некорректен:", err)
+		return false
+	}
+	return true
+}
